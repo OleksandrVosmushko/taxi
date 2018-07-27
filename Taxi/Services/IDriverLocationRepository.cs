15a237d678047caf5643461b2934a508d9ccfcc8
Oleksandr Vosmushko <sasha.vosmushko@gmail.com>
2018-07-27 15:30:00 +0300

    driver coordinate manipulations

--- Taxi/Services/IDriverLocationRepository.cs
+++ Taxi/Services/IDriverLocationRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Google.Common.Geometry;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,10 +10,12 @@ namespace Taxi.Services
     {
         bool UpdateUser(Guid uid, double lon, double lat);
 
-        void AddUser(Guid uid, double lon, double lat);
+        bool AddUser(Guid uid, double lon, double lat);
 
         List<Guid> Search(double lon, double lat, int radiusMeters);
 
         bool RemoveUser(Guid uid);
+
+        S2LatLng GetDriverLocation(Guid driverId); 
     }
 }

a586e37d7289e530181c0639a88ccd27d420d78e
Oleksandr Vosmushko <sasha.vosmushko@gmail.com>
2018-07-27 13:12:08 +0300

    push

---
+++ Taxi/Controllers/LocationController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taxi.Controllers
+{
+    public class LocationController
+    {
+    }
+}

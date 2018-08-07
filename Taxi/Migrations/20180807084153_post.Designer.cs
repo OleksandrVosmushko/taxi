7708379a46be996eab4708d08ce15bc0678add08
Oleksandr Vosmushko <sasha.vosmushko@gmail.com>
2018-08-07 11:50:54 +0300

    merge

--- Taxi/Migrations/20180807084153_post.Designer.cs
+++ Taxi/Migrations/20180807084153_post.Designer.cs
@@ -11,9 +11,10 @@ using Taxi.Data;
 namespace Taxi.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180807084153_post")]
+    partial class post
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
